f7fafcfa94b1b452c0f4aa5b61515da031b64489
Jacqueline Almeida <jacquelinealmeida@engefoto.com.br>
2025-07-09 16:30:52 -0300

    first commit

---
+++ app/controllers/api/v1/accommodations_controller.rb
@@ -0,0 +1,48 @@
+module Api
+  module V1
+    class AccommodationsController < ApplicationController
+      before_action :set_accommodation, only: %i[show update destroy]
+
+      def index
+        accommodations = Accommodation.all
+        render json: accommodations
+      end
+
+      def show
+        render json: @accommodation
+      end
+
+      def create
+        accommodation = Accommodation.new(accommodation_params)
+        if accommodation.save
+          render json: accommodation, status: :created
+        else
+          render json: accommodation.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        if @accommodation.update(accommodation_params)
+          render json: @accommodation
+        else
+          render json: @accommodation.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @accommodation.destroy
+        head :no_content
+      end
+
+      private
+
+      def set_accommodation
+        @accommodation = Accommodation.find(params[:id])
+      end
+
+      def accommodation_params
+        params.require(:accommodation).permit(:name, :description, :price, :location)
+      end
+    end
+  end
+end
